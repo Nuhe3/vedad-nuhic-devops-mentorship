665bf7ce3e5b30d5afff3774849e2bad074e4029
Nuhe3 <vedad.nuhic@edu.fit.ba>
2023-03-13 11:54:00 +0100

    adding screenshtos and scripts

---
+++ week-3/chapter14-scripts/scripts/05-chapter14-reading-scripts.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# Testing the $0 parameter
+#
+echo The zero parameter is set to: $0
+
+# bash 05-chapter14-reading-scripts.sh
---
+++ week-3/chapter14-scripts/scripts/13-chapter14-grabbing-param.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# testing $* and $@
+#
+echo
+echo "Using the \$* method: $*"
+echo
+echo "Using the \$@ method: $@"
+
+# ./13-chapter14-grabbing-param.sh vedad dragan boris dzenan
